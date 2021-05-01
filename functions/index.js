bf1b0a1378eccc4ad2d3e750cc8103e7d20e4c75
Carlos Rojas <carlos.rojas@localhost>
2021-05-01 12:39:30 -0500

    Updating functions

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,25 @@
-const functions = require("firebase-functions");
-const prpl = require("prpl-server");
-const express = require("express");
-const rendertron = require("rendertron-middleware");
-const minify = require("express-minify");
-const compression = require("compression");
+const functions = require('firebase-functions');
+const prpl = require('prpl-server');
+const express = require('express');
+const rendertron = require('rendertron-middleware');
+const minify = require('express-minify');
+const compression = require('compression');
 
 const app = express();
 
 const rendertronMiddleware = rendertron.makeMiddleware({
-  proxyUrl: "https://render-tron.appspot.com/render",
-  injectShadyDom: true
+	proxyUrl: 'https://render-tron.appspot.com/render',
+	injectShadyDom: true,
 });
 
 app
-  .use(compression())
-  .use(minify())
-  .use((req, res, next) => {
-    req.headers["host"] = "mike-posner.firebaseapp.com";
-    return rendertronMiddleware(req, res, next);
-  });
+	.use(compression())
+	.use(minify())
+	.use((req, res, next) => {
+		req.headers['host'] = 'carlosrojas-dev.firebaseapp.com';
+		return rendertronMiddleware(req, res, next);
+	});
 
-app.get("/*", prpl.makeHandler("./build", require("./build/polymer.json")));
+app.get('/*', prpl.makeHandler('./build', require('./build/polymer.json')));
 
 exports.app = functions.https.onRequest(app);
